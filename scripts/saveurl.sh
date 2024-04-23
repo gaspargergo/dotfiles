49b9f94a1636bb9f5b2573c6b5b0a3f85bd97934
Gergő Gáspár <gaspar.gergo@gmail.com>
2024-04-23 09:30:45 +0200

    Change saveurl to download videos instead of streaming them

--- scripts/saveurl.sh
+++ scripts/saveurl.sh
@@ -4,7 +4,7 @@ url=$(grep $1 "Link:" | cut -d' ' -f2)
 
 if [[ $(echo $url | grep -c "youtube") -eq 1 ]]
 then
-	mpv $url
+	yt-dlp --embed-subs --restrict-filenames -f 'best[height<=720]' -o '/home/gergo/videos/%(title)s.%(ext)s' $url
 else
 	echo $url >> ~/sitesToRead/readinglist.txt
 fi
