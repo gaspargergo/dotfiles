1b65bb6c236a7017650597825e6ea55b8aaa7d39
Gergő Gáspár <gaspar.gergo@gmail.com>
2024-11-04 19:11:35 +0100

    turn on dark mode earlier, since it's winter

--- scripts/check_time.sh
+++ scripts/check_time.sh
@@ -3,10 +3,10 @@
 hour=$(date "+%-H")
 theme=$(cat ~/.config/theme)
 
-if [[ $hour -lt 19 && $hour -gt 6 && $theme = "dark" ]]
+if [[ $hour -lt 17 && $hour -gt 6 && $theme = "dark" ]]
 then
 	~/scripts/chtheme.sh "light"
-elif [[ ( $hour -gt 19 && $theme = "light" ) || ( $hour -lt 6 && $theme = "light" ) ]]
+elif [[ ( $hour -gt 17 && $theme = "light" ) || ( $hour -lt 6 && $theme = "light" ) ]]
 then
 	~/scripts/chtheme.sh "dark"
 fi
