d9e357f11687046cebf70ae9ee3f27dd645c1246
Gergő Gáspár <gaspar.gergo@gmail.com>
2024-12-08 17:31:33 +0100

    go darktheme earlier

--- scripts/check_time.sh
+++ scripts/check_time.sh
@@ -3,10 +3,10 @@
 hour=$(date "+%-H")
 theme=$(cat ~/.config/theme)
 
-if [[ $hour -lt 17 && $hour -gt 6 && $theme = "dark" ]]
+if [[ $hour -lt 16 && $hour -gt 6 && $theme = "dark" ]]
 then
 	~/scripts/chtheme.sh "light"
-elif [[ ( $hour -gt 17 && $theme = "light" ) || ( $hour -lt 6 && $theme = "light" ) ]]
+elif [[ ( $hour -gt 16 && $theme = "light" ) || ( $hour -lt 6 && $theme = "light" ) ]]
 then
 	~/scripts/chtheme.sh "dark"
 fi
