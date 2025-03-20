cec04cdd46efbf42bf9bd297a5e52a2042e9512b
Gergő Gáspár <gaspar.gergo@gmail.com>
2025-03-20 16:17:40 +0100

    change to more summery time

--- scripts/check_time.sh
+++ scripts/check_time.sh
@@ -3,10 +3,10 @@
 hour=$(date "+%-H")
 theme=$(cat ~/.config/theme)
 
-if [[ $hour -lt 16 && $hour -gt 6 && $theme = "dark" ]]
+if [[ $hour -lt 18 && $hour -gt 6 && $theme = "dark" ]]
 then
 	~/scripts/chtheme.sh "light"
-elif [[ ( $hour -gt 16 && $theme = "light" ) || ( $hour -lt 6 && $theme = "light" ) ]]
+elif [[ ( $hour -gt 18 && $theme = "light" ) || ( $hour -lt 6 && $theme = "light" ) ]]
 then
 	~/scripts/chtheme.sh "dark"
 fi
