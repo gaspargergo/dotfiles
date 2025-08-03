3620886a69d8d08d4aeffaefc287e77aa8809b14
Gergő Gáspár <gaspar.gergo@gmail.com>
2025-08-03 08:52:26 +0200

    made improvements to saveurl script, now does yt,gemini, and html, works with urlview as well

--- scripts/saveurl.sh
+++ scripts/saveurl.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
-url=$(grep $1 "Link:" | cut -d' ' -f2)
+url=$1
+echo $url
+
+echo $url
 
 if [[ $(echo $url | grep -c "youtube") -eq 1 ]]
 then
 	yt-dlp --embed-subs --force-ipv4 --restrict-filenames -f "((bv*[fps>30]/bv*)[height<=720]/(wv*[fps>30]/wv*)) + ba / (b[fps>30]/b)[height<=720]/(w[fps>30]/w)" -o '/home/gergo/videos/%(title)s.%(ext)s' $url
-
+elif [[ $(echo $url | grep -c "gemini://") -eq 1 ]]
+then
+	gemget -d ~/sitesToRead/ $url
 else
-	echo $url >> ~/sitesToRead/readinglist.txt
+	if [[ $(echo $url | rev | cut -c1) = "/" ]] #if no page name, use last part of url. otherwise we would end up with a bunch of index.html
+	then
+		wget --progress=bar -P ~/sitesToRead/ -E --default-page=$(echo $url | rev | cut -d'/' -f2 |rev) $url
+	else
+		wget --progress=bar -P ~/sitesToRead/ -E $url
+	fi
 fi
 
