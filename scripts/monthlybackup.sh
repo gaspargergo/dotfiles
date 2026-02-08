2647b006917715e9a7ae5c3706db76970cb8a375
Gergő Gáspár <gaspar.gergo@gmail.com>
2026-02-08 15:10:53 +0100

    extra things to back up in script

--- scripts/monthlybackup.sh
+++ scripts/monthlybackup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 drive=$(lsblk -l | tac | fzf | cut -d ' ' -f1)
 sudo mount /dev/$drive /run/mount/
-sudo rsync -av --delete ~/ /run/mount/backup/
+sudo rsync -av --delete ~/ /run/mount/backup/home/
+sudo rsync -av --delete /etc/ /run/mount/backup/etc/
+sudo rsync -av --delete /usr/ /run/mount/backup/usr/
 sudo umount /dev/$drive
 
 echo "Backup complete!"
