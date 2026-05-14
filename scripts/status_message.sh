d960730638bc544dbd2eeb6dcfd23019e591945c
Gergő Gáspár <gaspar.gergo@gmail.com>
2026-05-14 11:55:16 +0200

    clean up status output

--- scripts/status_message.sh
+++ scripts/status_message.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e $(acpi)"\n"$(date) | xmessage -default okay -font '-*-Helvetica-*-r-normal-*-18-*-*-*-*-*-*-*' -timeout 5 -file -
+echo -e "Battery:"$(acpi -b | cut -d',' -f2)"\n"$(date "+%Y.%m.%d. (%a) %H:%M") | xmessage -default okay -font '-*-Helvetica-*-r-normal-*-18-*-*-*-*-*-*-*' -timeout 5 -file -
